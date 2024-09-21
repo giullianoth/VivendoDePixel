24cf39d431eb027e71696a101d665e12548b0202
Giulliano <llthguimaraes@gmail.com>
2024-09-21 14:44:52 -0300

    ajuste nas imagens da descrição

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,11 +6,6 @@ const countHour = document.querySelector(".j_count_hour")
 const countMinute = document.querySelector(".j_count_minute")
 const countSecond = document.querySelector(".j_count_second")
 
-const imagesToReplace = [...document.querySelectorAll(".j_image_to_replace")].map(element => element.cloneNode(true))
-const singleImage = (container) => container.querySelector(".j_image_to_replace")
-const imageContainers = document.querySelectorAll(".j_image_wrapper")
-const imageInfoContainers = document.querySelectorAll(".j_image_info_wrapper")
-
 // COUNTDOWN
 countHour.innerText = String(remainingHours).padStart(2, "0")
 countMinute.innerText = String(remainingMinutes).padStart(2, "0")
@@ -36,24 +31,4 @@ let countdown = setInterval(() => {
     countSecond.innerText = String(remainingSeconds).padStart(2, "0")
 
     remainingSeconds--
-}, 1000);
-
-// REPLACE IMAGES
-imageContainers.forEach((container, index) => {
-    if (window.innerWidth >= 992) {
-        container.append(imagesToReplace[index])
-        singleImage(imageInfoContainers[index]).remove()
-    }
-})
-
-window.addEventListener("resize", () => {
-    imageContainers.forEach((container, index) => {
-        if (window.innerWidth >= 992) {
-            container.append(imagesToReplace[index])
-            singleImage(imageInfoContainers[index]) && singleImage(imageInfoContainers[index]).remove()            
-        } else {
-            imageInfoContainers[index].append(imagesToReplace[index])
-            singleImage(container) && singleImage(container).remove()
-        }
-    })
-})
+}, 1000)
